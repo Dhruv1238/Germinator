ff1157d2af72af5f623903e1bd29bfa299c6afaf
Dhruv1238 <dhruv4075@gmail.com>
2023-10-23 23:43:22 +0530

    init: Firebase Conf

--- germinator/lib/main.dart
+++ germinator/lib/main.dart
@@ -9,12 +9,16 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        title: 'Germinator',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Germinator'),
+            backgroundColor: Colors.green,
+          ),
+          body: Center(
+            child: Text('Hello World'),
+          ),
+        ));
   }
 }
