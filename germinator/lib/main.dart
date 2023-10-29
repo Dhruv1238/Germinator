1b385b1b569fe03cb5d00d671d9462604097c97f
Dhruv1238 <dhruv4075@gmail.com>
2023-10-29 13:02:07 +0530

    feat: landing page beta

--- germinator/lib/main.dart
+++ germinator/lib/main.dart
@@ -12,7 +12,7 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: "Germinator",
-      home: LandingWidget(),
+      home: Landing(),
       debugShowCheckedModeBanner: false,
     );
   }
--- germinator/lib/screens/landing.dart
+++ germinator/lib/screens/landing.dart
@@ -1,20 +1,80 @@
 import 'package:flutter/material.dart';
 
-class LandingWidget extends StatelessWidget {
-  const LandingWidget({Key? key}) : super(key: key);
-
+class Landing extends StatelessWidget {
+  const Landing({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Germinator'),
-          centerTitle: true,
-          backgroundColor: Colors.green,
-        ),
-        body: const Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text('Landing Page'),
+      // ),
+      body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 50),
+              child: Text(
+                'Make your Life',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topCenter,
+            child: Container(
+              margin: EdgeInsets.only(top: 100),
+              child: Text(
+                'Greener!',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.green,
+                ),
+              ),
+            ),
+          ),
+          Center(
+            child:
+                Image.network('https://i.ibb.co/bLC56gz/germinator-Splash.png'),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin:
+                  EdgeInsets.only(bottom: 150), // Adjust the value as needed
+              child: ElevatedButton(
+                onPressed: () {
+                  print('Button Pressed');
+                },
+                child: Text('Button'),
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  textStyle: MaterialStateProperty.all<TextStyle>(
+                    TextStyle(fontSize: 20),
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    EdgeInsets.symmetric(horizontal: 100, vertical: 25),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(100),
+                      side: BorderSide(color: Colors.green),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+        
       ),
     );
   }
