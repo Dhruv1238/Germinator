13547c11a814fa347566f78d664ffd27c9505363
Dhruv1238 <dhruv4075@gmail.com>
2023-10-31 00:33:31 +0530

    My Plants Page Revamped

--- germinator/lib/screens/plant.dart
+++ germinator/lib/screens/plant.dart
@@ -19,6 +19,8 @@ class _PlantPageState extends State<PlantPage> {
         'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg'),
     Plant('Cactus', 'Every 14 days',
         'https://images.pexels.com/photos/2132224/pexels-photo-2132224.jpeg'),
+    Plant('Cactus', 'Every 14 days',
+        'https://images.pexels.com/photos/2132224/pexels-photo-2132224.jpeg'),
   ];
 
   void addPlant() {
@@ -28,41 +30,46 @@ class _PlantPageState extends State<PlantPage> {
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My Plants'),
-        backgroundColor: Colors.green,
-      ),
-      body: ListView.builder(
-        itemCount: plants.length,
-        itemBuilder: (context, index) {
-          return Padding(
+@override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('My Plants'),
+      backgroundColor: Colors.green,
+    ),
+    body: ListView.builder(
+      itemCount: plants.length,
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.lightGreen[100],
+          margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          child: Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0),
             child: ListTile(
               contentPadding: const EdgeInsets.all(16.0),
               leading: Image.network(plants[index].imageUrl),
               title: Text(
                 plants[index].name,
-                style: const TextStyle(fontSize: 18.0),
+                style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
               ),
               subtitle: Text(
                 'Watering Time: ${plants[index].wateringTime}',
                 style: const TextStyle(fontSize: 16.0),
               ),
             ),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        // onPressed: addPlant,
-        onPressed: () {
-          Navigator.pushNamed(context, '/plantcare');
-        },
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-      ),
-    );
-  }
+          ),
+        );
+      },
+    ),
+    
+    floatingActionButton: FloatingActionButton(
+      // onPressed: addPlant,
+      onPressed: () {
+        Navigator.pushNamed(context, '/plantcare');
+      },
+      child: Icon(Icons.add),
+      backgroundColor: Colors.green,
+    ),
+  );
+}
 }
